47022e7411bd1d628c195b1455490377df1fa40f
TaiNguyen <tainguyen05082002@gmail.com>
2025-04-27 12:09:38 +0700

    first commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Đảm bảo bao gồm các file .js, .jsx, .ts, .tsx
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
