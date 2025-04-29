ee40ba4eaf1a640b159b2c264bd63acb310ab0c0
TaiNguyen <tainguyen05082002@gmail.com>
2025-04-29 19:50:58 +0700

    UI Product details

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -1,10 +1,10 @@
-import { IoMdClose } from 'react-icons/io';
-import CartContents from '../Cart/CartContents';
+import { IoMdClose } from "react-icons/io";
+import CartContents from "../Cart/CartContents";
 
 const CartDrawer = ({ drawerOpen, toggleCartDrawer }) => {
   return (
     <div
-      className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[30rem] h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${drawerOpen ? 'translate-x-0' : 'translate-x-full'}`}
+      className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[30rem] h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${drawerOpen ? "translate-x-0" : "translate-x-full"}`}
     >
       {/* Close Button */}
       <div className="flex justify-end p-4">
@@ -24,7 +24,7 @@ const CartDrawer = ({ drawerOpen, toggleCartDrawer }) => {
           Checkout
         </button>
         <p className="text-sm tracking-tighter text-gray-500 mt-2 text-center">
-          Shipping, taxes, and discount codes calculated at checkout.{' '}
+          Shipping, taxes, and discount codes calculated at checkout.
         </p>
       </div>
     </div>
