07831179c72633b58b64a232916e6b4b5e70e6fd
TaiNguyen <tainguyen05082002@gmail.com>
2025-05-01 11:45:59 +0700

    handle filter fields

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -15,7 +15,7 @@ const Profile = () => {
           </div>
           {/* Right Section: Orders  table */}
           <div className="w-full md:w-2/3 lg:w-3/4">
-          <MyOrdersPage/>
+            <MyOrdersPage />
           </div>
         </div>
       </div>
