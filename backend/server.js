cee18694da2312ba999b8e50dce3f32ff185bb9b
TaiNguyen <tainguyen05082002@gmail.com>
2025-05-02 23:20:54 +0700

    Register and Login user

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 
 const app = express();
-app.use(express.json());
-app.use(cors());
+app.use(express.json()); // Xử lý dữ liệu JSON từ body của request
+app.use(cors()); // Cho phép CORS
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,6 +18,9 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("Welcome to Rabbit API!");
 });
+
+// API Routes
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
