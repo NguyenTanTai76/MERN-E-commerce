dac382c16de4e3d5ba65244a4eb1d30a3beef53d
TaiNguyen <tainguyen05082002@gmail.com>
2025-05-02 16:33:59 +0700

    Admin Dashboard Page

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import {
   HiOutlineUser,
   HiOutlineShoppingBag,
   HiBars3BottomRight,
-} from 'react-icons/hi2';
-import SearchBar from './SearchBar';
-import CartDrawer from '../Layout/CartDrawer';
-import { IoMdClose } from 'react-icons/io';
+} from "react-icons/hi2";
+import SearchBar from "./SearchBar";
+import CartDrawer from "../Layout/CartDrawer";
+import { IoMdClose } from "react-icons/io";
 
 const Navbar = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
@@ -61,6 +61,12 @@ const Navbar = () => {
 
         {/* Right - Icons */}
         <div className="flex items-center space-x-4">
+          <Link
+            to="/admin"
+            className="block bg-black px-2 rounded text-sm text-white"
+          >
+            Admin 
+          </Link>
           <Link to="/profile" className="hover:text-black">
             <HiOutlineUser className="h-6 w-6 text-gray-700" />
           </Link>
@@ -88,7 +94,7 @@ const Navbar = () => {
 
       {/* Mobile Navigation */}
       <div
-        className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3 h-full bg-white shadow-lg transform transition-transform duration-300 z-50 ${navDrawerOpen ? 'translate-x-0' : '-translate-x-full'}`}
+        className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3 h-full bg-white shadow-lg transform transition-transform duration-300 z-50 ${navDrawerOpen ? "translate-x-0" : "-translate-x-full"}`}
       >
         <div className="flex justify-end p-4">
           <button onClick={toggleNavDrawer}>
@@ -97,7 +103,7 @@ const Navbar = () => {
         </div>
         <div className="p-4">
           <h2 className="text-xl font-semibold mb-4">Menu</h2>
-          <nav className='space-y-4'>
+          <nav className="space-y-4">
             <Link
               to="#"
               onClick={toggleNavDrawer}
